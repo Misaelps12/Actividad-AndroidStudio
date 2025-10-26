918ec038263ff16613915060fe86ee275d860621
Misael <misael.oyarzun@gmail.com>
2025-10-26 17:09:00 -0300

    Proyecto Android

---
+++ app/src/main/java/com/imagen/appimagen/MainActivity.java
@@ -0,0 +1,65 @@
+package com.imagen.appimagen;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private RecyclerView recyclerView;
+    private ImageAdapter imageAdapter;
+    private List<Uri> imageUriList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSelectImage = findViewById(R.id.btnSelectImage);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // Configurar el RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        imageAdapter = new ImageAdapter(this, imageUriList);
+        recyclerView.setAdapter(imageAdapter);
+
+
+
+        // Evento para seleccionar imágenes
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+    }
+
+    // Método para seleccionar imágenes desde la galería
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+            imageUriList.add(imageUri);  // Añadir la imagen seleccionada a la lista
+            imageAdapter.notifyDataSetChanged();  // Notificar al adaptador para actualizar el RecyclerView
+        }
+    }
+}
+
+
+
